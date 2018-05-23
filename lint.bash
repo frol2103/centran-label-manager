1979ff1f18b17b91d2b708acf8cc319de73cb7c3
Damien Kech <damien.kech@centran.be>
2018-05-23 12:43:29 +0200

    lint

--- lint.bash
+++ lint.bash
@@ -5,12 +5,13 @@ set -o pipefail
 
 DIR=$(cd $(dirname $0) && pwd)
 
+pwd
 ls $DIR
 
 docker run \
-  -v ${URBAIN_ROOT:-$DIR}/src :/src \
+  -v ${$DIR}/ :/ \
   -u $(id -u)\
   --dns 10.23.142.11 \
   --dns 10.20.128.201 \
   bigbang.finbel.intra:5000/npm-angular:0.4 \
-  sh -c 'npm run lint '
+  sh -c 'npm install && npm run lint '
