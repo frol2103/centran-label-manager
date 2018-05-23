09bd57736d36e91cbed3524748bf212d61074cad
Damien Kech <damien.kech@centran.be>
2018-05-23 12:50:25 +0200

    lint

--- lint.bash
+++ lint.bash
@@ -8,7 +8,10 @@ DIR=$(cd $(dirname $0) && pwd)
 pwd
 ls $DIR
 
+echo ${URBAIN_ROOT:-$DIR}
+
 docker run \
+  -v ${URBAIN_ROOT:-$DIR} :/src \
   -u $(id -u)\
   --dns 10.23.142.11 \
   --dns 10.20.128.201 \
