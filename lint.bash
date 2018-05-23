dd8d46d777be0e6ad5cbb772ed9c0b8e8af275ca
Damien Kech <damien.kech@centran.be>
2018-05-23 12:39:22 +0200

    change lint.bash

--- lint.bash
+++ lint.bash
@@ -5,12 +5,12 @@ set -o pipefail
 
 DIR=$(cd $(dirname $0) && pwd)
 
+ls $DIR
+
 docker run \
-  -v ${URBAIN_ROOT:-$DIR}/ :/src \
+  -v ${URBAIN_ROOT:-$DIR}/src :/src \
   -u $(id -u)\
   --dns 10.23.142.11 \
   --dns 10.20.128.201 \
   bigbang.finbel.intra:5000/npm-angular:0.4 \
   sh -c 'npm run lint '
-
-rsync -r $DIR/gui/dist/ $DIR/web/src/main/webapp
