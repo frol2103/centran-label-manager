35fc0f8613da7b47d2f4a3157a9cfdcf603b4fd4
Damien Kech <damien.kech@centran.be>
2018-05-23 13:12:22 +0200

    linting

--- lint.bash
+++ lint.bash
@@ -11,9 +11,9 @@ ls $DIR
 echo ${URBAIN_ROOT:-$DIR}
 
 docker run \
-  -v ${URBAIN_ROOT:-$DIR}/src :/ \
+  -v $DIR:/src \
   -u $(id -u)\
   --dns 10.23.142.11 \
   --dns 10.20.128.201 \
   bigbang.finbel.intra:5000/npm-angular:0.4 \
-  sh -c 'npm install && npm run lint '
+  sh -c 'npm install && npm run lint'
