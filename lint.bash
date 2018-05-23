d896174b552989a04485bcb876c3d538a49fb9f5
Damien Kech <damien.kech@centran.be>
2018-05-23 12:52:02 +0200

    lint

--- lint.bash
+++ lint.bash
@@ -11,7 +11,7 @@ ls $DIR
 echo ${URBAIN_ROOT:-$DIR}
 
 docker run \
-  -v ${URBAIN_ROOT:-$DIR} :/src \
+  -v ${URBAIN_ROOT:-$DIR}/src :/ \
   -u $(id -u)\
   --dns 10.23.142.11 \
   --dns 10.20.128.201 \
